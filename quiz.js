e42d915aaaa1ed9d2dc966a6de0c4afb0a008a84
Vidhi Nair <74731487+VidhiNair@users.noreply.github.com>
2021-05-26 11:20:21 +0100

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,17 @@
+class Quiz{
+    consturctor(){}
+
+getState(){
+    
+}
+    display(){
+this.title.html("Snake & Spider BiteIfy! Please answer the following:")
+}
+
+}
+
+
+
+function quiz(){
+
+}
